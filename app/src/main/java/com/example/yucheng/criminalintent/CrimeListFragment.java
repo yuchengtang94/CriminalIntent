1699310bccf2ea25c80dd061f54669d0698ece82
yuchengtang94 <yuchengtang94@gmail.com>
2018-03-30 14:57:56 -0400

    version 9

--- app/src/main/java/com/example/yucheng/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/example/yucheng/criminalintent/CrimeListFragment.java
@@ -10,6 +10,7 @@ import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -49,6 +50,7 @@ public class CrimeListFragment extends Fragment{
 
         private TextView mTitleTextView;
         private TextView mDateTextView;
+        private ImageView mSolvedImageView;
 
         private Crime mCrime;
 
@@ -56,6 +58,7 @@ public class CrimeListFragment extends Fragment{
             mCrime = crime;
             mTitleTextView.setText(mCrime.getTitle());
             mDateTextView.setText(mCrime.getDate().toString());
+            mSolvedImageView.setVisibility(crime.isSolved() ? View.VISIBLE : View.GONE);
         }
 
         public CrimeHolder(LayoutInflater inflater, ViewGroup parent) {
@@ -63,6 +66,7 @@ public class CrimeListFragment extends Fragment{
 
             mTitleTextView = (TextView) itemView.findViewById(R.id.crime_title);
             mDateTextView = (TextView) itemView.findViewById(R.id.crime_date);
+            mSolvedImageView = (ImageView)itemView.findViewById(R.id.crime_solved);
             itemView.setOnClickListener(this);
         }
 
